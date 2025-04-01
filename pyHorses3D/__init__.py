9323ab409a44f52da8b9d04cce077e1323efbb7d
Jon Errasti <jon44errasti@gmail.com>
2025-04-01 18:11:50 +0200

    Update package initialization and version
       - Add module docstring describing package purpose
       - Export all core classes and new examples module
       - Add CLI module to exports
       - Update version to 0.2.0

--- pyHorses3D/__init__.py
+++ pyHorses3D/__init__.py
@@ -1,7 +1,18 @@
 # pyHorses3D/__init__.py
 
+"""
+pyHorses3D - Python interface for the Horses3D CFD solver
+
+This package provides tools to interact with the Horses3D CFD solver,
+manage control files, process simulation results, and visualize data.
+"""
+
 from .horses3d import Horses3D
 from .control import Horses3DControl
 from .plot import Horses3DPlot
 from .mesh import Horses3DMesh
 from .solution import Horses3DSolution
+from . import examples
+from . import cli
+
+__version__ = '0.2.0'
